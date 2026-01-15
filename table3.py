9e6e211608d6881546216049374fbd48d2829076
aadhya298 <hope.aashi_02@outlook.com>
2026-01-15 22:40:37 +0530

    table commit

---
+++ table3.py
@@ -0,0 +1,23 @@
+import sqlite3
+import pandas as pd
+
+conn= sqlite3.connect("d.db")
+print("Connection established")
+conn.commit()
+
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS CLASS(
+        ROLL_NO INTEGER PRIMARY KEY,
+        NAME TEXT NOT NULL,
+        CLASS INTEGER DEFAULT(11),
+        EMAIL_ID TEXT UNIQUE    
+    )   
+""")
+
+conn.execute("INSERT OR IGNORE INTO CLASS(ROLL_NO,NAME,EMAIL_ID) VALUES(1,'AADHYA','aadhya.11@yahoo.com')")
+conn.execute("INSERT OR IGNORE INTO CLASS(ROLL_NO,NAME,EMAIL_ID) VALUES(2,'ANAMIKA','anamika.22@rediff.com')")
+conn.execute("INSERT OR IGNORE INTO CLASS(ROLL_NO,NAME,EMAIL_ID) VALUES(3,'AVNI','avni.41@yahoo.com')")
+conn.execute("INSERT OR IGNORE INTO CLASS(ROLL_NO,NAME,EMAIL_ID) VALUES(4,'SAGAR','sagar.08@rediff.com')")
+
+df= pd.read_sql("SELECT * FROM CLASS", conn)
+print(df)
